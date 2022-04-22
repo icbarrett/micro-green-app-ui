6260bf55b88ee540f616399c04e95451426bd263
Cristina <cristina.gratton@gmail.com>
2022-04-22 12:05:04 -0500

    nav bar and logo set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+//import nav bar and container components for react boostrap
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+//import Logo
+import logo from './assets/mglogo4.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Hello React
-      </header>
-    </div>
+    <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img 
+          alt="Logo"
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"/>{' '}
+        Ingi's Microgreens
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#inventory">Inventory</Nav.Link>
+          <Nav.Link href="#orders">Orders</Nav.Link>
+          <Nav.Link href="#calendar">Calendar</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 }
 
