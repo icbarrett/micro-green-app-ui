f447a842ba36553c9768004b7de940bfc8fbd536
Cristina <cristina.gratton@gmail.com>
2022-04-25 17:11:22 -0500

    sepearte Nav component, streamline imports

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React, { useState } from 'react';
 
-import './App.css';
-//import nav bar and container components for react boostrap
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-
-//import Logo
-import logo from './assets/mglogo4.png';
+//import nav bar
+import NavigationBar from './NavigationBar.js';
+import { Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <Navbar bg="light" expand="lg">
-    <Container>
-      <Navbar.Brand href="#home">
-        <img 
-          alt="Logo"
-          src={logo}
-          width="30"
-          height="30"
-          className="d-inline-block align-top"/>{' '}
-        Ingi's Microgreens
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#inventory">Inventory</Nav.Link>
-          <Nav.Link href="#orders">Orders</Nav.Link>
-          <Nav.Link href="#calendar">Calendar</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+   <NavigationBar></NavigationBar>
   );
 }
 
