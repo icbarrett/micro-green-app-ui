4ce6e96b20525fdd6373ea34a16860fac554b426
lynnf <lynn.fiferjr@gmail.com>
2022-05-02 11:57:18 -0500

    updated task card

--- src/components/CalendarAssets/DayView.js
+++ src/components/CalendarAssets/DayView.js
@@ -3,28 +3,17 @@ import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './Calendar.css'
 
+function DayView({ day, date }) {
 
-function DayView({ day }) {
-
-    const getDay = () => {
-        switch (day) {
-            case 0:
-                day = 'Sunday'
-                break;
-        
-            default:
-                break;
-        }
-    }
-    
+   
 
     return (
         <Card className="main-card">
             <Card.Body>
                 <div>
-                    <Card.Title className="md-0 font-weight-bold">Daily Tasks for {day}</Card.Title>
+                    <Card.Title >Daily Tasks for {day}</Card.Title>
                 </div>
-                <Card.Text className="text-secondary"></Card.Text>
+                <Card.Title >{date}</Card.Title>
             </Card.Body>
         </Card>
     );
--- src/components/NewCalendar.js
+++ src/components/NewCalendar.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css"
 import { Container } from "react-bootstrap";
-import DayView from "./CalendarAssets/DayView.js";
+import DayView from "./calendarAssets/DayView.js";
 
 function NewCalendar() {
     const [date, setDate] = useState(new Date());
     const prevDate = new Date();
+    let day = ""
 
 
     const onChange = date => {
@@ -14,12 +15,53 @@ function NewCalendar() {
     }
 
 
+
+    // if (date.getDay() === 0) {
+    //     day = 'Sunday'
+    // }
+
+    switch (date.getDay()) {
+        case 0:
+            day = 'Sunday'
+            break;
+
+        case 1:
+            day = 'Monday'
+            break;
+
+        case 2:
+            day = 'Tuesday'
+            break;
+
+        case 3:
+            day = 'Wednesday'
+            break;
+
+        case 4:
+            day = 'Thursday'
+            break;
+
+        case 5:
+            day = 'Friday'
+            break;
+
+        case 6:
+            day = 'Saturday'
+            break;
+
+        default:
+            break;
+    }
+
+
+
+
     return (
         <>
-            <h1 className="heading">Today's Date</h1>
-            <h2 className="selected-date">{prevDate.toDateString()}</h2>
+            <h1 className="heading">Today's Date {prevDate.toDateString()}</h1>
+            <h2 className="selected-date"></h2>
             <Container>
-                <div className="dayView"><DayView day={date.toDateString()} />
+                <div className="dayView"><DayView day={day} date={date.toLocaleDateString()} />
                 </div>
                 <div className="calendar-container">
                     <Calendar onChange={onChange} date={date} />
