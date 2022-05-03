3310d0ce45b16c798c8d28fb62abf3a8ea829f37
lynnf <lynn.fiferjr@gmail.com>
2022-05-02 20:32:31 -0500

    css formatting

--- src/components/NewCalendar.js
+++ src/components/NewCalendar.js
@@ -18,49 +18,39 @@ function NewCalendar() {
         case 0:
             day = 'Sunday'
             break;
-
         case 1:
             day = 'Monday'
             break;
-
         case 2:
             day = 'Tuesday'
             break;
-
         case 3:
             day = 'Wednesday'
             break;
-
         case 4:
             day = 'Thursday'
             break;
-
         case 5:
             day = 'Friday'
             break;
-
         case 6:
             day = 'Saturday'
             break;
-
         default:
             break;
     }
 
-
-
-
     return (
         <>
             <h1 className="heading">Today's Date {prevDate.toDateString()}</h1>
-            <h2 className="selected-date"></h2>
-            <Container>
+            
+            
                 <div className="dayView"><DayView day={day} date={date.toLocaleDateString()} />
                 </div>
                 <div className="calendar-container">
-                    <Calendar onChange={onChange} date={date} />
+                    <Calendar onChange={onChange} date={date}  />
                 </div>
-            </Container>
+            
         </>
     );
 }
