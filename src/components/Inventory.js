9fb2eda49c495cd29d591f6246711b7722e7a655
Cristina <cristina.gratton@gmail.com>
2022-05-05 14:05:05 -0500

    add table styling to inventory page

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,47 @@
 import React from 'react'
 
+//import table component styling
+import Table from 'react-bootstrap/Table'
+
 const Inventory = () => {
   return (
-    <div>This is the Inventory Page</div>
+    <div>
+      <h1>Inventory</h1>
+      <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>LOT NO.</th>
+      <th>SEED NAME</th>
+      <th>COUNT</th>
+      <th>WEIGHT(g)</th>
+      <th>STATUS</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Larry the Bird</td>
+      <td>@twitter</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</Table>
+    </div>
   )
 }
 
