353394c371dd3ba415ae7186a94561fee1fd70c2
McDev <71328737+mcdev8@users.noreply.github.com>
2022-05-05 18:04:28 -0500

    api example (#9)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Home = () => {
+  
+  const [helloWorld, setHelloWorld] = useState();
+
+
+  useEffect(() => {
+    // runs 1 time because it's in a `useEffect`
+    fetchHelloWorld();
+  }, []);
+
+  const fetchHelloWorld = () => {
+    fetch('http://localhost:8080/')
+      .then((response) => {
+        // If an object is returned from API you could do
+        // return response.json()
+        return response.text();
+      })
+      .then((text) => {
+        setHelloWorld(text);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div>This is the Home Page</div>
+    <div>
+      This is the Home Page.
+      { helloWorld } 
+    </div>
   )
 }
 
