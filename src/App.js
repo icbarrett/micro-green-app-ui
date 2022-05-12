bbe35744b1a6cc69bfc3be30223e60fbe4e9ceb9
lynnf <lynn.fiferjr@gmail.com>
2022-05-12 04:47:17 -0500

    moved NewCalendar component into Calendar page, added function to update task in DayTask,

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@
 
 //import nav bar component
 import NavigationBar from './components/NavigationBar.js';
-//calendar component
-import NewCalendar from './components/NewCalendar.js';
-// DayView import
-import DayView from './components/CalendarAssets/DayView.js';
 
 function App (){
   return (
