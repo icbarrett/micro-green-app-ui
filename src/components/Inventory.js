3f075659107baa93e623ac8f96c5fa11770e8fb1
Cristina <cristina.gratton@gmail.com>
2022-05-12 16:51:01 -0500

    change table values

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 
 //import table component styling
 import {Table, Button, Form} from 'react-bootstrap';
 
+
 const Inventory = () => {
+  const [users, setUsers] = useState([])
+
+  const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await response.json()
+    setUsers(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div>
       <div class="inventory">
@@ -11,10 +24,8 @@ const Inventory = () => {
         <div class="item">
           <h4>SORT BY</h4>
           <Form.Select >
-            <option>Lot No.</option>
             <option>Seed Name</option>
             <option>Weight</option>
-            <option>Status</option>
           </Form.Select>
         </div>
         <Button variant="dark" class="item" id="addSeedsBtn">ADD SEEDS</Button>
@@ -22,34 +33,22 @@ const Inventory = () => {
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>LOT NO.</th>
             <th>SEED NAME</th>
-            <th>COUNT</th>
             <th>WEIGHT(g)</th>
-            <th>STATUS</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>1</td>
             <td>Dandelion</td>
             <td>200</td>
-            <td>3</td>
-            <td>Available</td>
           </tr>
           <tr>
-            <td>2</td>
             <td>Amaranth</td>
             <td>300</td>
-            <td>5</td>
-            <td>Available</td>
           </tr>
           <tr>
-            <td>3</td>
             <td>Basil Genovese</td>
             <td>400</td>
-            <td>1.5</td>
-            <td>Planted</td>
           </tr>
         </tbody>
       </Table>
