5f95dd80abfcf8478d42c23d532d6ad4e9ab1bfc
Cristina <cristina.gratton@gmail.com>
2022-05-12 17:16:43 -0500

    setup for linking api

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -5,17 +5,26 @@ import {Table, Button, Form} from 'react-bootstrap';
 
 
 const Inventory = () => {
-  const [users, setUsers] = useState([])
-
-  const fetchData = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const data = await response.json()
-    setUsers(data)
-  }
+  const [seeds, setSeeds] = useState([])
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    // runs 1 time because it's in a `useEffect`
+    fetchSeeds();
+  }, []);
+
+  const fetchSeeds = () => {
+    fetch('http://localhost:3000/inventory/view')
+      .then((response) => {
+        return response.json();
+      })
+      .then((text) => {
+        setSeeds(text);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
 
   return (
     <div>
@@ -37,20 +46,15 @@ const Inventory = () => {
             <th>WEIGHT(g)</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>Dandelion</td>
-            <td>200</td>
+        {seeds.map((seed) => (
+          <tbody>
+          <tr key={seed.id}>
+            <td>{seed.seedName}</td>
+            <td>{seed.qty}</td>
           </tr>
-          <tr>
-            <td>Amaranth</td>
-            <td>300</td>
-          </tr>
-          <tr>
-            <td>Basil Genovese</td>
-            <td>400</td>
-          </tr>
-        </tbody>
+          </tbody>
+        ))
+        }
       </Table>
     </div>
   )
