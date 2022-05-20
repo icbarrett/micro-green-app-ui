0e5da05f8e67c0edd09488e8e90230682d7f9138
Sharon Konki <sharonkonki@gmail.com>
2022-05-19 20:42:02 -0500

    cleaned up code

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,15 +7,6 @@ import { Nav, Navbar, Container } from 'react-bootstrap';
 //import Logo
 import logo from '../assets/ingismicrogreenstransparentV1.png';
 
-// //import react-router-dom to display pages & navigate between them
-// import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
-// //import pages to create route for home, inventory, orders, calendar
-// import Home from "../components/Home.js";
-// import Inventory from "../components/Inventory.js";
-// import Orders from "../components/Orders.js";
-// import NewCalendar from "../components/NewCalendar.js";
-
 //wrap react bootstrap element in <LinkContainer> to make it behave like a react router
 import { LinkContainer } from 'react-router-bootstrap';
 
