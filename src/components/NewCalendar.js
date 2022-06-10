da6ca116bc9e3fff4a58907bfdb6a5be1ec5a7ec
lynnf <lynn.fiferjr@gmail.com>
2022-06-10 10:37:17 -0500

    code cleanup for Calendar

--- src/components/NewCalendar.js
+++ src/components/NewCalendar.js
@@ -2,54 +2,22 @@ import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css"
 import DayView from "./CalendarAssets/DayView.js";
-import axios from "axios";
-// import data from './anotherDummy.json';
 
 function NewCalendar() {
     const [date, setDate] = useState(new Date());
-    
     const prevDate = new Date();
-    // let tasks = []
     let day = "";
     const [todaysDate, setTodaysDate ] = useState('');
     const [clickedDay, setClickedDay] = useState(date.toISOString().split('T', 1).toString())
-   
-
-    
-
-    // fetch data from api
-    // useEffect(() => {
-    //   fetch("http://localhost:8080/task")
-    //   .then(response => response.json())
-    //   .then(data => setData(data)) 
-    // }, [])
-    
 
     useEffect(() => {
         setTodaysDate(date.toISOString().split('T', 1).toString())
     }, [])
     
-
-    // for (let index = 0; index < data.length; index++) {
-        
-    //     if (data[index].dueDate === clickedDay) {
-    //         tasks.push(data[index])
-            
-    //     }
-        
-    // }
-
-    
-    
-    
     const handleChange = date => {
         setDate(date);
         setClickedDay(date.toISOString().split('T', 1).toString())
-        
-        
     }
-
-    
 
     switch (date.getDay()) {
         case 0:
@@ -82,7 +50,6 @@ function NewCalendar() {
             <h1 className="heading">Today's Date {prevDate.toDateString()}</h1>
                 <DayView className="dayView" day={day} date={date.toLocaleDateString()} clickedDay={clickedDay}/>
                 <Calendar className="calendar" onChange={handleChange}  />
-                
         </div>
     );
 }
