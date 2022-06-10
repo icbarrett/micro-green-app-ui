f7e27a06f9848b2fbe369a360dfea5b6fe8f4a3d
cristinagratton <cristina.gratton@gmail.com>
2022-06-10 10:59:01 -0500

    react line edit

--- src/components/CalendarAssets/DayView.js
+++ src/components/CalendarAssets/DayView.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React from 'react';
 import { Card } from 'react-bootstrap';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'
