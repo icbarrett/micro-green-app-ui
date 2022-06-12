3efec29fb2af65363d8ab7fa6c3f9381344e471d
lynnf <lynn.fiferjr@gmail.com>
2022-06-12 11:02:00 -0500

    finished calendar

--- src/components/CalendarAssets/Task.js
+++ src/components/CalendarAssets/Task.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 
 
 const Task = ({ id, task, complete }) => {
@@ -16,11 +16,6 @@ const Task = ({ id, task, complete }) => {
             complete: !checked
         })
      };
-
-    useEffect(() => { 
-     
-    }, []);
-     
 
     return (
         <div>
